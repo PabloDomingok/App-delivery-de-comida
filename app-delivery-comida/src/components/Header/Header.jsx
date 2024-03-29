c7424afcc44fa00f661e5516aa573771d541f267
Pablo Domingo <pablodomingo1993@gmail.com>
2024-03-29 18:13:05 -0300

    Se han arreglado algunos error y se ha creado le header

---
+++ app-delivery-comida/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header' class="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Experience the convenience of food delivery at your fingertips with our app. Order now and enjoy delicious meals delivered straight to your door.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- app-delivery-comida/src/pages/Home/Home.jsx
+++ app-delivery-comida/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
 const Home = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
