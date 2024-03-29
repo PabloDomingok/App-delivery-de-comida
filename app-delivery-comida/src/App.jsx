5c5d3b7753b97187ab26e3c789633db5d5ae80f8
Pablo Domingo <pablodomingo1993@gmail.com>
2024-03-29 16:56:48 -0300

    Se han creado los routers en la página App.jsx

--- app-delivery-comida/src/App.jsx
+++ app-delivery-comida/src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import Navbar from './components/Nabvar/Navbar'
+import React from 'react';
+import Navbar from './components/Nabvar/Navbar'; // Asumiendo la ruta correcta al componente Navbar
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home'; // Asumiendo la ruta correcta al componente Home
+import Cart from './pages/Cart/Cart'; // Asumiendo la ruta correcta al componente Cart
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
