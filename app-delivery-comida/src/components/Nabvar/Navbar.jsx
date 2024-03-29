2f811ccca81039a206471c7af66345205269834a
Pablo Domingo <pablodomingo1993@gmail.com>
2024-03-29 16:20:18 -0300

    Se han arreglado algunos errores

--- app-delivery-comida/src/components/Nabvar/Navbar.jsx
+++ app-delivery-comida/src/components/Nabvar/Navbar.jsx
@@ -1,20 +1,19 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets';
 
 const Navbar = () => {
+    const [menu, setMenu] = useState("home");
 
-const [menu,setMenu]=useState("home");
-    
-  return (
-    <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
-        <ul className="navbar-menu">
-            <li onClick={()=>setMenu("home")} className={menu=="home"?"active":""}>Home</li>
-            <li onClick={()=>setMenu("menu")} className={menu=="menu"?"active":""}>Menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu=="mobile-app"?"active":""}>Mobile app</li>
-            <li onClick={()=>setMenu("contact-us")} className={menu=="cantact-us"?"active":""}>Contact us</li>
-        </ul>
+    return (
+        <div className='navbar'>
+            <img src={assets.logo} alt="" className="logo" />
+            <ul className="navbar-menu">
+                <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+                <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
+                <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile app</li>
+                <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact us</li>
+            </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="" />
                 <div className="navbar-search-icon">
@@ -23,10 +22,8 @@ const [menu,setMenu]=useState("home");
                 <div className="dot"></div>
                 <button>Sign in</button>
             </div>
-        
-      
-    </div>
-  )
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
