b2a58304b047d53677e0ba5188995fa774cdc2b6
Pablo Domingo <pablodomingo1993@gmail.com>
2024-04-02 16:26:40 -0300

    Se ha agregado la sección de descarga de las apps móviles

--- app-delivery-comida/src/pages/Home/Home.jsx
+++ app-delivery-comida/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 const Home = () => {
 
   const [category,setCategory] =useState("All");
@@ -11,6 +12,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
