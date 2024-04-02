60b2148d3ee2c8a56a6f6c07477335ed49143d6c
Pablo Domingo <pablodomingo1993@gmail.com>
2024-04-01 23:37:20 -0300

    Se ha creado el componente de la lista de comidas

---
+++ app-delivery-comida/src/context/StoreContext.jsx
@@ -0,0 +1,40 @@
+import React, { createContext, useState, useEffect } from "react"
+import { food_list } from "../assets/assets"
+
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev,[itemId]: 1 }))
+        } else {
+            setCartItems((prev) => ({ ...prev,[itemId]:prev[itemId] + 1 }))
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems]);
+
+    const contextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart // Corregido el nombre de la función
+    }
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    );
+}
+
+export default StoreContextProvider;
