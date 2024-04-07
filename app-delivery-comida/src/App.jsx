96b1604a598239fceb2553f3006b5c397894787f
Pablo Domingo <pablodomingo1993@gmail.com>
2024-04-07 00:51:28 -0300

    Agregado componente de inicio de sesión / registro

--- app-delivery-comida/src/App.jsx
+++ app-delivery-comida/src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Nabvar/Navbar'; // Asumiendo la ruta correcta al componente Navbar
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home'; // Asumiendo la ruta correcta al componente Home
 import Cart from './pages/Cart/Cart'; // Asumiendo la ruta correcta al componente Cart
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup';
 
 const App = () => {
+
+  const [showLogin,setShowLogin] =useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin} />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
